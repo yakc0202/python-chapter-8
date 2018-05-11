c4246ce1bd94f8e515a0a2d03da941f8f4b5f718
yakc0202 <39176082+yakc0202@users.noreply.github.com>
2018-05-11 10:20:21 +0900

    Add files via upload

---
+++ sel.py
@@ -0,0 +1,13 @@
+score=int(input("점수: "))
+deg={10:'A',9:'A',8:'B',7:'C',6:'D',5:'F',4:'F',3:'F',2:'F',1:'F',0:'F'}
+if score>=0 and score<=100:
+    print(score, ":", deg[score//10])
+else:
+    print("입력 가능한 점수 범위는 0~100입니다.")
+
+score=int(input("점수: "))
+deg={10:'A',9:'A',8:'B',7:'C',6:'D',5:'F',4:'F',3:'F',2:'F',1:'F',0:'F'}
+if score>=0 and score<=100:
+    print(score, ":", deg[score//10])
+else:
+    print("입력 가능한 점수 범위는 0~100입니다.")
